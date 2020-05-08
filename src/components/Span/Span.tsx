dc797972cd49f09c4c2ac0cc01fd1083af2e191b
Lucas Marandat <lucas.mrdt@gmail.com>
2020-05-08 10:47:28 +0200

    add mdx span

---
+++ src/components/Span/Span.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const Span = styled.span`
+  color: ${(p) => p.theme.colors.articleText};
+  transition: ${(p) => p.theme.colorModeTransition};
+`;
+
+export default Span;
